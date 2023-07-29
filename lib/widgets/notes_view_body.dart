97d11fce472e497991e6503cabf5baab038fef0c
Ahmed Algzery <algzeryahmed14@gmail.com>
2023-07-29 10:45:35 +0300

    upgrade

--- lib/widgets/notes_view_body.dart
+++ lib/widgets/notes_view_body.dart
@@ -20,10 +20,10 @@ class _NotesViewBodyState extends State<NotesViewBody> {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.all(24.0),
+    return const Padding(
+      padding: EdgeInsets.all(24.0),
       child: Column(
-        children: const [
+        children: [
           CustomAppBar(
             title: 'Note',
             icon: Icons.search,
